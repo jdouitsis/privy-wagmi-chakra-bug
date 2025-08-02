d4c30a647a3fa33ad90e2d1d69e3410664fa10a1
James Douitsis <jamesdouitsis@gmail.com>
2025-08-02 12:39:49 -0400

    Still broken

--- app/dummy/page.tsx
+++ app/dummy/page.tsx
@@ -1,13 +1,10 @@
 'use client';
 
-import {LinkIcon} from '@chakra-ui/icons';
 import {
   MenuItem,
   Avatar,
   Button,
-  chakra,
   HStack,
-  Image,
   Menu,
   MenuButton,
   MenuList,
@@ -15,10 +12,10 @@ import {
   VStack,
   Divider,
 } from '@chakra-ui/react';
-import {PlusIcon, UserIcon} from 'lucide-react';
+import {PlusIcon} from 'lucide-react';
 import {useAccount} from 'wagmi';
 
-import {useLinkAccount, usePrivy, useWallets} from '@privy-io/react-auth';
+import {useLinkAccount, useWallets} from '@privy-io/react-auth';
 
 const shortAddress = (address?: string) => {
   if (!address) return '';
@@ -28,9 +25,51 @@ const shortAddress = (address?: string) => {
 const Page: React.FC = () => {
   const {linkWallet} = useLinkAccount();
 
+  const selectedWallet = useAccount();
+  const {wallets} = useWallets();
+
   return (
     <VStack w="full" maxW="md" mx="auto" my={10}>
-      <Selector />
+      <Menu direction="rtl" placement="bottom-end">
+        <MenuButton>{selectedWallet ? 'Selected' : 'None selected'}</MenuButton>
+        <MenuList
+          px="2"
+          minW="min-content"
+          bg="g.primary.oxford.700"
+          borderColor="g.neutral.metallic.dark"
+        >
+          {wallets.map(
+            (wallet) =>
+              wallet.linked === true && (
+                <MenuItem
+                  bg={
+                    selectedWallet?.address === wallet.address
+                      ? 'g.primary.oxford.600'
+                      : 'g.primary.oxford.700'
+                  }
+                  _hover={{bg: 'g.primary.oxford.600'}}
+                  gap="2"
+                  p="2"
+                  alignItems="center"
+                  key={wallet.address}
+                >
+                  <Avatar size="xs" src={wallet.meta.icon} />
+                  <Text>{shortAddress(wallet.address)}</Text>
+                </MenuItem>
+              ),
+          )}
+          <MenuItem
+            bg="g.primary.oxford.700"
+            _hover={{bg: 'g.primary.oxford.600'}}
+            p="2"
+            gap="2"
+            alignItems="center"
+          >
+            <PlusIcon fill="blue" />
+            Add Wallet
+          </MenuItem>
+        </MenuList>
+      </Menu>
 
       <Divider />
       <HStack>
@@ -47,51 +86,3 @@ const Page: React.FC = () => {
 };
 
 export default Page;
-
-const Selector = () => {
-  const selectedWallet = useAccount();
-  const {wallets} = useWallets();
-
-  return (
-    <Menu direction="rtl" placement="bottom-end">
-      <MenuButton>{selectedWallet ? 'Selected' : 'None selected'}</MenuButton>
-      <MenuList
-        px="2"
-        minW="min-content"
-        bg="g.primary.oxford.700"
-        borderColor="g.neutral.metallic.dark"
-      >
-        {wallets.map(
-          (wallet) =>
-            wallet.linked === true && (
-              <MenuItem
-                bg={
-                  selectedWallet?.address === wallet.address
-                    ? 'g.primary.oxford.600'
-                    : 'g.primary.oxford.700'
-                }
-                _hover={{bg: 'g.primary.oxford.600'}}
-                gap="2"
-                p="2"
-                alignItems="center"
-                key={wallet.address}
-              >
-                <Avatar size="xs" src={wallet.meta.icon} />
-                <Text>{shortAddress(wallet.address)}</Text>
-              </MenuItem>
-            ),
-        )}
-        <MenuItem
-          bg="g.primary.oxford.700"
-          _hover={{bg: 'g.primary.oxford.600'}}
-          p="2"
-          gap="2"
-          alignItems="center"
-        >
-          <PlusIcon fill="blue" />
-          Add Wallet
-        </MenuItem>
-      </MenuList>
-    </Menu>
-  );
-};
