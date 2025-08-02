7de3801bb69477d443bda1555b20e375f4f4fdb8
James Douitsis <jamesdouitsis@gmail.com>
2025-08-02 12:41:43 -0400

    Still broken

--- app/dummy/page.tsx
+++ app/dummy/page.tsx
@@ -2,7 +2,6 @@
 
 import {
   MenuItem,
-  Avatar,
   Button,
   HStack,
   Menu,
@@ -12,7 +11,6 @@ import {
   VStack,
   Divider,
 } from '@chakra-ui/react';
-import {PlusIcon} from 'lucide-react';
 import {useAccount} from 'wagmi';
 
 import {useLinkAccount, useWallets} from '@privy-io/react-auth';
@@ -29,57 +27,24 @@ const Page: React.FC = () => {
   const {wallets} = useWallets();
 
   return (
-    <VStack w="full" maxW="md" mx="auto" my={10}>
-      <Menu direction="rtl" placement="bottom-end">
+    <VStack>
+      <Menu>
         <MenuButton>{selectedWallet ? 'Selected' : 'None selected'}</MenuButton>
-        <MenuList
-          px="2"
-          minW="min-content"
-          bg="g.primary.oxford.700"
-          borderColor="g.neutral.metallic.dark"
-        >
+        <MenuList>
           {wallets.map(
             (wallet) =>
               wallet.linked === true && (
-                <MenuItem
-                  bg={
-                    selectedWallet?.address === wallet.address
-                      ? 'g.primary.oxford.600'
-                      : 'g.primary.oxford.700'
-                  }
-                  _hover={{bg: 'g.primary.oxford.600'}}
-                  gap="2"
-                  p="2"
-                  alignItems="center"
-                  key={wallet.address}
-                >
-                  <Avatar size="xs" src={wallet.meta.icon} />
+                <MenuItem key={wallet.address}>
                   <Text>{shortAddress(wallet.address)}</Text>
                 </MenuItem>
               ),
           )}
-          <MenuItem
-            bg="g.primary.oxford.700"
-            _hover={{bg: 'g.primary.oxford.600'}}
-            p="2"
-            gap="2"
-            alignItems="center"
-          >
-            <PlusIcon fill="blue" />
-            Add Wallet
-          </MenuItem>
         </MenuList>
       </Menu>
 
       <Divider />
       <HStack>
-        <Button
-          onClick={() => {
-            setTimeout(linkWallet, 0);
-          }}
-        >
-          Link Wallet
-        </Button>
+        <Button onClick={linkWallet}>Link Wallet</Button>
       </HStack>
     </VStack>
   );
